bbbe82f2b4d7ecb6ec01583d73e968af92016e9c
Абдула Хизриев <khizriev.abdula31@gmail.com>
2023-12-31 07:51:42 +0300

    remove

--- src/com/intellekta/generics/middleearth/Main.java
+++
@@ -1,9 +0,0 @@
-package com.intellekta.generics.middleearth;
-
-import com.intellekta.generics.middleearth.units.mordorUnits.mordorInfantry.UrukHai;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
